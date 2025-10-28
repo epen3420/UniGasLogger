864fd62fd1a43228ebe4e3a3db1eedd227710b45
epen3420 <epen3420@gmail.com>
2025-10-28 12:09:14 +0900

    Unity側の必要なスクリプトを持ってくる

---
+++ Scripts/Editor/GasSettingEditor.cs
@@ -0,0 +1,73 @@
+using UniGasLogger.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace UniGasLogger.Editor
+{
+    public class GasSettingEditor : EditorWindow
+    {
+        [MenuItem("Tools/LogSenderSetting")]
+        private static void OpenWindow()
+        {
+            GetWindow<GasSettingEditor>("LogSender Settings");
+        }
+
+        private GasSettings settings;
+        private string newDeployId;
+        private string newAuthToken;
+        private string newSheetId;
+
+        private void OnEnable()
+        {
+            settings = GasSettingsService.LoadSettings();
+
+            if (settings == null)
+            {
+                settings = GasSettingsService.CreateSettings();
+            }
+
+            newDeployId = settings.DeployId;
+            newAuthToken = settings.AuthToken;
+            newSheetId = settings.SheetId;
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("LogSender Settings", EditorStyles.boldLabel);
+
+            if (settings == null)
+            {
+                GUILayout.Label("Settings asset could not be loaded.", EditorStyles.helpBox);
+                if (GUILayout.Button("Retry Load/Resolve Paths"))
+                {
+                    OnEnable();
+                }
+                return;
+            }
+
+            EditorGUI.BeginChangeCheck();
+
+            GUILayout.Label("Deploy ID");
+            newDeployId = GUILayout.TextField(newDeployId);
+
+            GUILayout.Label("Auth Token");
+            newAuthToken = GUILayout.TextField(newAuthToken);
+
+            GUILayout.Label("Sheet ID");
+            newSheetId = GUILayout.TextField(newSheetId);
+
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(settings, "Change LogSender Settings");
+
+                GasSettingsService.UpdateSettings(settings, newDeployId, newAuthToken, newSheetId);
+            }
+
+            if (GUILayout.Button("Save Settings to Disk"))
+            {
+                GasSettingsService.SaveSettingsToDisk();
+            }
+        }
+    }
+}
---
+++ Scripts/Runtime/GasServiceManager.cs
@@ -0,0 +1,61 @@
+using UniGasLogger.Data;
+using UniGasLogger.Service;
+using UniGasLogger.Http;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UniGasLogger
+{
+    public class GasServiceManager : MonoBehaviour
+    {
+        public static GasServiceManager Instance;
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+
+        private GasLoggerService loggerService;
+        private RankingClientService rankingService;
+
+
+        private void Start()
+        {
+            var gasSettings = GasSettingsService.LoadSettings();
+            var httpClient = new GasHttpClient(gasSettings);
+
+            loggerService = new GasLoggerService(httpClient);
+            rankingService = new RankingClientService(httpClient);
+        }
+
+        public async Task SendLog<T>(T datas, string sheetName = null)
+        {
+            await loggerService.SendLog(datas, sheetName);
+        }
+
+        public async Task SendLog(Dictionary<string, object> datas, string sheetName = null)
+        {
+            await loggerService.SendLog(datas, sheetName);
+        }
+
+        public async Task<RankingResponse> GetTop5Ranking(string sheetName = null)
+        {
+            return await rankingService.GetTop5Ranking(sheetName);
+        }
+
+        public async Task<ScoreRankResponse> GetScoreRanking(BigInteger score, string sheetName = null)
+        {
+            return await rankingService.GetScoreRank(score, sheetName);
+        }
+    }
+}
