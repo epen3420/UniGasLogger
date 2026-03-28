f2551741ad7f1462487dab6ed776ee17b8113d89
epen <epen3420@gmail.com>
2026-03-28 20:35:27 +0900

    Release/v1.3.0 (#11)
    * feat: 有効かどうかのプロパティを追加
    
    * feat: 設定のウィンドウに無効化にする選択肢を追加
    
    * feat: 設定をセーブする際に有効/無効も保存
    
    * feat: ログが無効の場合Managerを無効化
    
    * feat: 無効だった場合、他のプロパティの変更を無効化
    
    * chore/change-directory-composition (#9)
    
    * chore: 今まで存在したテスト用スクリプト
    
    * chore: .asmdefの編集
    
    UniGasClient -> UniGasLogger
    
    * chore: import tmp
    
    * feat: sendlog test
    
    * fix: Settingsウィンドウの有効/無効の表示を修正
    
    * chore: コメントの追記
    
    * refactor: 命名の変更
    
    * feat: 設定をSOから編集できないようにした
    
    * feat: 設定をプレイ中変更できないようにした
    
    * chore: サンプルファイルの場所を変更
    
    * chore: upmにサンプルなどを表示
    
    * chore: Samples -> Samples~
    
    * chore: ログメッセージの変更
    
    * Update README.md

---
+++ UniGasLogger/Editor/GasSettingEditor.cs
@@ -0,0 +1,74 @@
+using UniGasLogger.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace UniGasLogger.Editor
+{
+    public class GasSettingEditor : EditorWindow
+    {
+        [MenuItem("Tools/LogSenderSetting")]
+        private static void OpenWindow()
+        {
+            GetWindow<GasSettingEditor>("LogSender Settings");
+        }
+
+        private GasSettings settings;
+        private bool newIsEnable;
+        private string newDeployId;
+        private string newAuthToken;
+        private string newSheetId;
+
+        private void OnEnable()
+        {
+            settings = GasSettingsService.LoadOrCreateSettings();
+
+            newIsEnable = settings.IsEnable;
+            newDeployId = settings.DeployId;
+            newAuthToken = settings.AuthToken;
+            newSheetId = settings.SheetId;
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("LogSender Settings", EditorStyles.boldLabel);
+
+            if (settings == null)
+            {
+                GUILayout.Label("Settings asset could not be loaded.", EditorStyles.helpBox);
+                if (GUILayout.Button("Retry Load/Resolve Paths"))
+                {
+                    OnEnable();
+                }
+                return;
+            }
+
+            EditorGUI.BeginChangeCheck();
+
+            GUILayout.Label("Disable Logging");
+            newIsEnable = GUILayout.Toggle(newIsEnable, "");
+
+            EditorGUI.BeginDisabledGroup(!newIsEnable);
+            GUILayout.Label("Deploy ID");
+            newDeployId = GUILayout.TextField(newDeployId);
+
+            GUILayout.Label("Auth Token");
+            newAuthToken = GUILayout.TextField(newAuthToken);
+
+            GUILayout.Label("Sheet ID");
+            newSheetId = GUILayout.TextField(newSheetId);
+            EditorGUI.EndDisabledGroup();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(settings, "Change LogSender Settings");
+
+                GasSettingsService.UpdateSettings(settings, newDeployId, newAuthToken, newSheetId, newIsEnable);
+            }
+
+            if (GUILayout.Button("Save Settings to Disk"))
+            {
+                GasSettingsService.SaveSettingsToDisk();
+            }
+        }
+    }
+}
---
+++ UniGasLogger/Runtime/GasServiceManager.cs
@@ -0,0 +1,78 @@
+using UniGasLogger.Data;
+using UniGasLogger.Service;
+using UniGasLogger.Http;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UniGasLogger
+{
+    public class GasServiceManager : MonoBehaviour
+    {
+        public static GasServiceManager Instance;
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+
+
+            var gasSettings = Resources.Load<GasSettings>("GasSettings");
+            if (!gasSettings.IsEnable)
+            {
+                Debug.LogWarning("[UniGasLogger]: UniGasLogger is disabled. if you were enable it please check 'Assets/Plugins/UniGasLogger'.");
+                enabled = false;
+                Destroy(this.gameObject);
+                return;
+            }
+            var httpClient = new GasHttpClient(gasSettings);
+
+            loggerService = new GasLoggerService(httpClient);
+            rankingService = new RankingClientService(httpClient);
+        }
+
+
+        private GasLoggerService loggerService;
+        private RankingClientService rankingService;
+
+
+        public async Task SendLog<T>(T datas, string sheetName = null)
+        {
+            if (datas == null)
+            {
+                Debug.LogError("datas is null. so can not send log.");
+                return;
+            }
+
+            await loggerService.SendLog(datas, sheetName);
+        }
+
+        public async Task SendLog(Dictionary<string, object> datas, string sheetName = null)
+        {
+            if (datas == null)
+            {
+                Debug.LogError("datas is null. so can not send log.");
+                return;
+            }
+
+            await loggerService.SendLog(datas, sheetName);
+        }
+
+        public async Task<RankingResponse> GetTopNRanking(int n, string sheetName = null)
+        {
+            return await rankingService.GetTopNRanking(n, sheetName);
+        }
+
+        public async Task<ScoreRankResponse> GetScoreRanking(BigInteger score, string sheetName = null)
+        {
+            return await rankingService.GetScoreRank(score, sheetName);
+        }
+    }
+}
